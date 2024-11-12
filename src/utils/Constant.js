85d263d31f331e988c899a07555a6fbbe26bba3d
A. Developers <hinaarooj978@gmail.com>
2024-11-12 20:55:20 +0500

    new

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,9 +1,9 @@
 module.exports = {
   // Local server
-  baseUrl: "http://localhost:4000/api/v1",
+  // baseUrl: "http://localhost:4000/api/v1",
   // Local server
   // Official server
-  // baseUrl: "https://blockguard-gzgm.onrender.com/api/v1",
+  baseUrl: "https://juscatering.onrender.com/api/v1",
   // baseUrl: "https://blockguard.onrender.com/api/v1",
   // Official server
 };
